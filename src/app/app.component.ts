d2e24de28880b610c4f0a8f7b9736e6f85681fdb
tunahan.berber <tunahan.berber@eba.gov.tr>
2025-01-13 12:08:33 +0300

    tema-change-button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NasaGalleryComponent } from './components/nasa-gallery/nasa-gallery.component';
-import { HeroSectionComponent } from './shared/hero-section/hero-section.component';
+import { HeroSectionComponent } from './shared/components/hero-section/hero-section.component';
+import { SharedModule } from './shared/shared.module';
 
 @Component({
   selector: 'app-root',
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { HeroSectionComponent } from './components/hero-section/hero-section.component';
 
 
 
@@ -8,7 +8,7 @@ import { HeroSectionComponent } from './hero-section/hero-section.component';
   declarations: [],
   imports: [
     CommonModule,
-    HeroSectionComponent
+    HeroSectionComponent,
   ],
   exports: [HeroSectionComponent]
 })
