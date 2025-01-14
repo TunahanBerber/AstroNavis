c8d056927b61a9114b724e29c8e75f161f6b6633
tunahan.berber <tunahan.berber@eba.gov.tr>
2025-01-14 15:09:21 +0300

    add-logo

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroSectionComponent } from './components/hero-section/hero-section.component';
 import { ThemeButtonsComponent } from './components/theme-buttons/theme-buttons.component';
+import { LogoComponent } from './components/logo/logo.component';
 
 
 
@@ -10,8 +11,9 @@ import { ThemeButtonsComponent } from './components/theme-buttons/theme-buttons.
   imports: [
     CommonModule,
     HeroSectionComponent,
-    ThemeButtonsComponent
+    ThemeButtonsComponent,
+    LogoComponent
   ],
-  exports: [HeroSectionComponent,ThemeButtonsComponent]
+  exports: [HeroSectionComponent,ThemeButtonsComponent,LogoComponent]
 })
 export class SharedModule { }
