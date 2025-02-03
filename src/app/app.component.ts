7c00f3109e111291133a15591515cd04523c6f40
tunahan.berber <tunahan.berber@eba.gov.tr>
2025-02-03 20:54:44 +0300

    mars-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,13 @@ import { HeroSectionComponent } from './shared/components/hero-section/hero-sect
 import { BlackHoleComponent } from './components/black-hole/black-hole.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { LogoComponent } from './shared/components/logo/logo.component';
+import { MarsRoverComponent } from './components/mars-rover/mars-rover.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NasaGalleryComponent, HeroSectionComponent,BlackHoleComponent,FooterComponent],
+  imports: [RouterOutlet, NasaGalleryComponent, HeroSectionComponent,BlackHoleComponent,FooterComponent,MarsRoverComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'], // Updated from styleUrl to styleUrls
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'AstroNavis';
